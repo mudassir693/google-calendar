f9e704561aa676d3b7b7c657743242f33e8f3014
Mudassir Siddiqui <59256774+mudassir693@users.noreply.github.com>
2025-01-09 01:07:55 +0500

    Update Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,35 +1,39 @@
-import React, { useEffect,useState,useContext } from 'react'
-import Day from '../components/day/Day'
-import {getMonth} from '../utils/functions'
-import { CalContext} from '../context/ProjectContext'
-import EventModal from '../components/eventModal/EventModal'
+import React, { useEffect, useState, useContext } from 'react';
+import Day from '../components/day/Day';
+import { getMonth } from '../utils/functions';
+import { CalContext } from '../context/ProjectContext';
+import EventModal from '../components/eventModal/EventModal';
+
 function Calendar() {
-    const [month,setMonth] = useState()
-    const {monthCtx,modalOpenCtx,setDayReRender} = useContext(CalContext)
-    useEffect(()=>{
-        let start = true
-        start&&console.log('month: ',getMonth())
-        setMonth(()=>getMonth(monthCtx))
-        setDayReRender((prev)=>!prev)
-        return ()=>{
-          start = false
+    const [currentMonth, setCurrentMonth] = useState();
+    const { monthCtx, isModalOpen, triggerDayReRender } = useContext(CalContext);
+
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted) {
+            console.log('currentMonth: ', getMonth());
+            setCurrentMonth(getMonth(monthCtx));
+            triggerDayReRender(prev => !prev);
         }
-      },[monthCtx])
-      console.log('month: ',month)
-  return (
-    <div className="relative w-100 h-[90vh] grid grid-cols-7 grid-rows-5">
-        {modalOpenCtx&&<EventModal />}
-        {month?.map((week,idx)=>(
-            <React.Fragment key={idx} className="">
-                {week?.map((day,id)=>(
-                    // <React.Fragment >
-                        <Day key={id} day={day} weekIdx={idx}/>
-                    // </React.Fragment>
-                ))}
-            </React.Fragment>
-        ))}
-    </div>
-  )
+        return () => {
+            isMounted = false;
+        };
+    }, [monthCtx, triggerDayReRender]);
+
+    console.log('currentMonth: ', currentMonth);
+
+    return (
+        <div className="relative w-full h-[90vh] grid grid-cols-7 grid-rows-5">
+            {isModalOpen && <EventModal />}
+            {currentMonth?.map((week, weekIdx) => (
+                <React.Fragment key={weekIdx}>
+                    {week?.map((day, dayIdx) => (
+                        <Day key={dayIdx} day={day} weekIdx={weekIdx} />
+                    ))}
+                </React.Fragment>
+            ))}
+        </div>
+    );
 }
 
-export default Calendar
+export default Calendar;
