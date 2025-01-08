60ece0cf271c79ee697b6b91813802caded95570
Mudassir Siddiqui <59256774+mudassir693@users.noreply.github.com>
2025-01-09 01:13:41 +0500

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,52 +1,54 @@
-import React,{useState,useContext,useEffect, useMemo} from 'react'
-import SmallCalendar from '../smallCalendar/SmallCalendar'
-import day from 'dayjs'
-import {CalContext} from '../../context/ProjectContext'
+import React, { useState, useContext, useEffect } from 'react';
+import SmallCalendar from '../smallCalendar/SmallCalendar';
+import day from 'dayjs';
+import { CalContext } from '../../context/ProjectContext';
 import SidebarEvent from './../sideBarEvent/SidebarEvent';
 
 function Sidebar() {
-    const {monthCtx,setMonthCtx,eventCtx} = useContext(CalContext)
+    const { monthCtx, setMonthCtx, eventCtx } = useContext(CalContext);
 
-    useEffect(()=>{
-        console.log('okay is it reload',eventCtx)
-    },[eventCtx])
+    useEffect(() => {
+        console.log('Event Context Updated: ', eventCtx);
+    }, [eventCtx]);
 
-    let handleNextDate = ()=>{
-        setMonthCtx((prev)=>prev+1)
-    }
-    let handlePrevDate = ()=>{
-        setMonthCtx((prev)=>prev-1)
-    }
-  return (
-    <div className="sidebar mx-3">
-        <div className="create-btn py-2 px-3 w-[fit-content] rounded-full fond-semibold drop-shadow-xl border border-gray-500 cursor-pointer">
-            create
-            <span className="ml-8">
-                <i class="fa-solid fa-caret-down"></i>
-            </span>
-        </div>
+    const handleNextMonth = () => {
+        setMonthCtx(prev => prev + 1);
+    };
 
-        <div className="w-[90%] my-5">
-            <div className="mx-2 flex items-center justify-between my-2">
-                {day(new Date(day().year(), monthCtx)).format('MMMM YYYY')}
-                <div className="schevrons flex items-center">
-                    <div onClick={handlePrevDate} className="right text-gray-700 mx-2 cursor-pointer">
-                        <i class="fa-solid fa-chevron-left"></i>
-                    </div>
-                    <div onClick={handleNextDate} className="right text-gray-700 mx-2 cursor-pointer">
-                        <i class="fa-solid fa-chevron-right"></i>
+    const handlePrevMonth = () => {
+        setMonthCtx(prev => prev - 1);
+    };
+
+    return (
+        <div className="sidebar mx-3">
+            <div className="create-btn py-2 px-3 w-fit rounded-full font-semibold drop-shadow-xl border border-gray-500 cursor-pointer">
+                Create
+                <span className="ml-8">
+                    <i className="fa-solid fa-caret-down"></i>
+                </span>
+            </div>
+
+            <div className="w-[90%] my-5">
+                <div className="mx-2 flex items-center justify-between my-2">
+                    {day(new Date(day().year(), monthCtx)).format('MMMM YYYY')}
+                    <div className="schevrons flex items-center">
+                        <div onClick={handlePrevMonth} className="left text-gray-700 mx-2 cursor-pointer">
+                            <i className="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div onClick={handleNextMonth} className="right text-gray-700 mx-2 cursor-pointer">
+                            <i className="fa-solid fa-chevron-right"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <SmallCalendar />
-            <div className="my-5">
-                {eventCtx.map(each => (
-                    <SidebarEvent event={each} />
-                ))}
+                <SmallCalendar />
+                <div className="my-5">
+                    {eventCtx.map((event, index) => (
+                        <SidebarEvent key={index} event={event} />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
