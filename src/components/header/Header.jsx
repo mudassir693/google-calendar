7dc62bc7b27512028b92e5be99331d5f33aa78fa
Discoursefy <162369940+Discoursefy@users.noreply.github.com>
2025-01-09 20:56:23 +0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,19 +5,23 @@ import day from 'dayjs';
 
 function Header() {
     const { monthCtx, setMonthCtx } = useContext(CalContext);
-    const [color, setColor] = useState(null)
 
-    const handleNextMonth = () => {
-        setMonthCtx(prev => prev + 1);
-    };
 
-    const handlePrevMonth = () => {
-        setMonthCtx(prev => prev - 1);
+    const [color, setColor] = useState("")
+
+    const handleMonthChange = (direction) => {
+        setMonthCtx(prev => prev + direction);
     };
 
     const handleToday = () => {
         setMonthCtx(day().month());
     };
+
+    // const handleToday = () => {
+    //     setMonthCtx(day().month());
+    // };
+
+    const formattedMonth = day(new Date(day().year(), monthCtx)).format('MMMM YYYY');
 
     return (
         <div className="header flex justify-between items-center py-3 px-8">
@@ -26,19 +30,28 @@ function Header() {
                     <img className="h-[40px] object-contain" src={CalendarIcon} alt="Calendar Icon" />
                     <span className="mx-2">Calendar</span>
                 </div>
-                <div onClick={handleToday} className="todayBtn border border-gray-100 rounded-lg text-gray-700 mx-4 py-2 px-2 font-medium text-sm cursor-pointer">
+                <div 
+                    onClick={handleToday} 
+                    className="todayBtn border border-gray-100 rounded-lg text-gray-700 mx-4 py-2 px-2 font-medium text-sm cursor-pointer"
+                >
                     Today
                 </div>
                 <div className="schevrons flex items-center mx-5">
-                    <div onClick={handlePrevMonth} className="left text-gray-700 mx-2 cursor-pointer">
+                    <div 
+                        onClick={() => handleMonthChange(-1)} 
+                        className="left text-gray-700 mx-2 cursor-pointer"
+                    >
                         <i className="fa-solid fa-chevron-left"></i>
                     </div>
-                    <div onClick={handleNextMonth} className="right text-gray-700 mx-2 cursor-pointer">
+                    <div 
+                        onClick={() => handleMonthChange(1)} 
+                        className="right text-gray-700 mx-2 cursor-pointer"
+                    >
                         <i className="fa-solid fa-chevron-right"></i>
                     </div>
                 </div>
                 <div className="monthname text-gray-700 text-xl">
-                    {day(new Date(day().year(), monthCtx)).format('MMMM YYYY')}
+                    {formattedMonth}
                 </div>
             </div>
         </div>
