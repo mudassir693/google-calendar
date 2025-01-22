684000f25a51959b9a97d5b3ef9a2d7da182d0db
Mudassir Siddiqui <59256774+mudassir693@users.noreply.github.com>
2025-01-22 20:30:25 +0500

    Update Day.jsx

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -57,7 +57,6 @@ function Day({ day, weekIdx }) {
                 </div>
             )}
             <div className={`absolute top-[50%] translate-x-[-50%] left-[50%] h-full flex-1 my-auto ${isToday(day) ? 'translate-y-[-20%]' : 'translate-y-[-10%]'}`}>
-                {console.log('hello)}
                 <div className={`${isToday(day) ? 'bg-blue-500 rounded-full w-full h-full text-white' : ''} flex justify-center items-center`}>
                     {day.format('DD')}
                 </div>
